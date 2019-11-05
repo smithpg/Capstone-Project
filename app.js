13b111af7bca6e122ce55097ca1d146d7c9f425f
Humza Ahmed <hsa5037@gmail.com>
2019-11-04 19:32:23 -0500

    DB/Google Authentication
    push the db and just have it log a basic SQL statement to the console. I'm also pushing the google authentication stuff that I have done so far, that will only work when deploying via gcloud sdk tools.

--- app.js
+++ app.js
@@ -3,10 +3,43 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 
 const { taskRouter } = require("./routes");
 
 const app = express();
+
+
+//Open Database in memory
+let db = new sqlite3.Database('./db/perro.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+
+//SQL Statement
+let sql = `SELECT * FROM Users`;
+
+//Log statement to console
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+  });
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
 
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
@@ -38,6 +71,20 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.json(err);
+});
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'));
+});
+
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
 
 module.exports = app;
