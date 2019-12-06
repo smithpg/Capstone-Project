a4cee2cf9bb2ac9304859f0160c60f9e718e3b8b
patrickgsmith <patrickgromersmith@gmail.com>
2019-12-05 19:28:00 -0800

    add mongo uri for prod

--- data/index.js
+++ data/index.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-// { DB_PASSWORD, DB_USER, DB_NAME } = process.env;
+const { DB_PASSWORD, DB_USER, DB_NAME, DB_DOMAIN } = process.env;
 
-// const connectionString =
-//     process.env.NODE_ENV !== "production"
-//         ? "mongodb://localhost:27017/perro"
-//         : `mongodb://${DB_USER}:${DB_PASSWORD}@{DOMAIN}/${DB_NAME}`;
-
-const connectionString = "mongodb://localhost:27017/perro";
+const connectionString =
+  process.env.NODE_ENV !== "production"
+    ? "mongodb://localhost:27017/perro"
+    : `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_DOMAIN}/${DB_NAME}`;
 
 module.exports.connection = mongoose.connect(connectionString, {
   useNewUrlParser: true, // Use new url parser instead of default deprecated one
