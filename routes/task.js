1dbaedde34c3919ec2d796af89aeeedc0eb978c0
patrickgsmith <patrickgromersmith@gmail.com>
2019-12-05 17:21:05 -0800

    refactored auth middleware

--- routes/task.js
+++ routes/task.js
@@ -5,49 +5,60 @@ const createError = require("http-errors");
 
 const { Task, Report } = require("../data");
 
-const { userHasPermission } = require("../middleware/auth");
+const authMiddleware = require("../middleware/auth");
 
 /**
  * POST to /task to create a new task
  */
-router.post("/", userHasPermission("EDIT"), async (req, res, next) => {
-  const { error } = validatetask(req.body);
-  if (error) return next(createError(400, error.details[0].message));
+router.post(
+  "/",
+  authMiddleware.userIsLoggedIn,
+  authMiddleware.userHasPermission("EDIT"),
+  async (req, res, next) => {
+    const { error } = validatetask(req.body);
+    if (error) return next(createError(400, error.details[0].message));
 
-  /**
-   *    Attempt to create the task
-   */
+    /**
+     *    Attempt to create the task
+     */
 
-  const newTask = await Task.create({
-    ...req.body,
-    project: req.params.project_id
-  });
+    const newTask = await Task.create({
+      ...req.body,
+      project: req.params.project_id
+    });
 
-  /**
-   *    Return success message
-   */
-  res.status(201).send(newTask);
-});
-
-router.put("/:task_id", userHasPermission("EDIT"), async (req, res, next) => {
-  const { error } = validatetask(req.body);
-  if (error) return next(createError(400, error.details[0].message));
-
-  try {
-    //Attempt to update the task
-    const task = await Task.findById(req.params.task_id);
-
-    Object.assign(task, req.body);
-
-    res.status(204).send();
-  } catch (error) {
-    return next(createError(500, error));
+    /**
+     *    Return success message
+     */
+    res.status(201).send(newTask);
   }
-});
+);
+
+router.put(
+  "/:task_id",
+  authMiddleware.userIsLoggedIn,
+  authMiddleware.userHasPermission("EDIT"),
+  async (req, res, next) => {
+    const { error } = validatetask(req.body);
+    if (error) return next(createError(400, error.details[0].message));
+
+    try {
+      //Attempt to update the task
+      const task = await Task.findById(req.params.task_id);
+
+      Object.assign(task, req.body);
+
+      res.status(204).send();
+    } catch (error) {
+      return next(createError(500, error));
+    }
+  }
+);
 
 router.delete(
   "/:task_id",
-  userHasPermission("ADMIN"),
+  authMiddleware.userIsLoggedIn,
+  authMiddleware.userHasPermission("ADMIN"),
   async (req, res, next) => {
     const task = Task.findById(req.params.task_id);
 
@@ -59,7 +70,8 @@ router.delete(
 
 router.post(
   "/:task_id/reports",
-  userHasPermission("EDIT"),
+  authMiddleware.userIsLoggedIn,
+  authMiddleware.userHasPermission("EDIT"),
   async (req, res, next) => {
     await Report.create({ ...req.body, task: req.params.task_id });
     res.status(201).send({ success: true });
@@ -68,7 +80,8 @@ router.post(
 
 router.put(
   "/:task_id/reports/:report_id",
-  userHasPermission("EDIT"),
+  authMiddleware.userIsLoggedIn,
+  authMiddleware.userHasPermission("EDIT"),
   async (req, res, next) => {
     await Report.findByIdAndUpdate(req.params.report_id, req.body);
     res.status(204).send();
